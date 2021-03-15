c3b4c0ae4f1b8da34d858a7e67e7b02555da4ede
DESKTOP-QUPUQ2M\Minja <mrcha993@gmail.com>
2021-03-15 18:50:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MojaAplikacija
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World! from the Tips4you");
+        }
+    }
+}
