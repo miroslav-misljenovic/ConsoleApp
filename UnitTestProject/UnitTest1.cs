496a723d9af260ed48f781ed315214d8390ad7e6
miroslav-misljenovic <mrcha993@gmail.com>
2021-04-20 15:17:54 +0200

    Updated test - should pass

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace UnitTestProject
     [TestClass]
     public class UnitTest1
     {
-        private const string Expected = "Hello World! ......";
+        private const string Expected = "Hello World! Testing Jenkins SCM polling...";
         [TestMethod]
         public void TestMethod1()
         {
